69c92f8aa2d2ff64ed25801d87f1e459a7cd4461
Snessy <A.King.per@Gmail.com>
2016-02-27 19:38:57 +0000

    Added weatherconnector call to main appplication point

--- src/com/snessy/main/Application.java
+++ src/com/snessy/main/Application.java
@@ -2,11 +2,14 @@ package com.snessy.main;
 import java.io.IOException;
 
 import com.snessy.auth.RedditConnector;
+import com.snessy.auth.WeatherConnector;
 
 public class Application {
 
 	public static void main(String[] args) throws IOException {
 		RedditConnector redditConnector = new RedditConnector();
 		redditConnector.connect("config.properties");
+		WeatherConnector weatherConnector = new WeatherConnector();
+		weatherConnector.connect();
 	}
 }
