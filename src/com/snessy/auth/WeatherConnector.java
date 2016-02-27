1f19615715d6af40ab5c4c84c818d2828909a653
Snessy <A.King.per@Gmail.com>
2016-02-27 19:38:08 +0000

    Added weather class with temp connection to London

---
+++ src/com/snessy/auth/WeatherConnector.java
@@ -0,0 +1,19 @@
+package com.snessy.auth;
+
+import java.io.IOException;
+
+import org.apache.http.ParseException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+
+public class WeatherConnector {
+	
+	public void connect() {
+		CloseableHttpResponse response = HttpHandler.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=44db6a862fba0b067b1930da0d769e98", null);
+		try {
+			System.out.println(EntityUtils.toString(response.getEntity()));
+		} catch (ParseException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
