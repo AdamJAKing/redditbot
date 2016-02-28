949818d48b8041635a05d1016fd514735cf01f5e
Adam King <a.king.per@Gmail.com>
2016-02-28 08:41:47 +0000

    Weather connector name requires a location and country

--- src/com/snessy/auth/WeatherConnector.java
+++ src/com/snessy/auth/WeatherConnector.java
@@ -8,8 +8,9 @@ import org.apache.http.util.EntityUtils;
 
 public class WeatherConnector {
 	
-	public void connect() {
-		CloseableHttpResponse response = HttpHandler.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=44db6a862fba0b067b1930da0d769e98", null);
+	public void connect(String location, String country) {
+		CloseableHttpResponse response = HttpHandler.get("http://api.openweathermap.org"
+				+ "/data/2.5/weather?q="+location+","+country+"&"+ "appid=44db6a862fba0b067b1930da0d769e98", null);
 		try {
 			System.out.println(EntityUtils.toString(response.getEntity()));
 		} catch (ParseException | IOException e) {
