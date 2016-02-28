68ddaf898654febacbf5705f1c8d85ef9c81c9c5
Adam King <a.king.per@Gmail.com>
2016-02-28 08:40:46 +0000

    Switch redditconnector to a more oop structure. Also fixed a logical check when attempting to connect to reddit

--- src/com/snessy/auth/RedditConnector.java
+++ src/com/snessy/auth/RedditConnector.java
@@ -1,10 +1,7 @@
 package com.snessy.auth;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Properties;
 
 import org.apache.http.ParseException;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -18,47 +15,55 @@ public class RedditConnector {
 	
 	public static final String USER_AGENT = "Snessabot-redditbotv1.0 by /u/Ihuntbacon";
 	
+	private String username;
+	private String password;
+	private String clientId;
+	private String clientSecret;
+	private String accessToken;
+	
+	public RedditConnector(String user, String pass, String clientId, String clientSecret) {
+		this.username = user;
+		this.password = pass;
+		this.clientId = clientId;
+		this.clientSecret = clientSecret;
+	}
+	
 	/**
 	 * Loads in the properties file and uses the data to connect the the reddit API
 	 * 
 	 * @param filePath Path to the properties file
 	 */
 	
-	public void connect(String filePath) {
+	public void connect() {
 		System.out.println("Attemping to connect to reddit...");
-		Properties props = new Properties();
-		
-		try {
-			props.load(new FileInputStream(new File(filePath)));
-		} catch (IOException ex) {
-			System.out.println("File: " + filePath + " cannot be read");
-			ex.printStackTrace();
-		}
 		
 		HashMap<String, String> headers = new HashMap<String, String>();
 		headers.put("User-Agent", USER_AGENT);
 		
 		HashMap<String, String> nameValuePairs = new HashMap<String, String>();
 		nameValuePairs.put("grant_type", "password");
-		nameValuePairs.put("username", props.getProperty("username"));
-		nameValuePairs.put("password", props.getProperty("password"));
+		nameValuePairs.put("username", username);
+		nameValuePairs.put("password", password);
 		
-		CloseableHttpResponse response = HttpHandler.post("https://www.reddit.com/api/v1/access_token", props.getProperty("clientId"), 
-				props.getProperty("clientSecret"), headers, nameValuePairs);
+		CloseableHttpResponse response = HttpHandler.post("https://www.reddit.com/api/v1/accasess_token", clientId, 
+				clientSecret, headers, nameValuePairs);
 		
-		if(response != null) {
+		if(response.getStatusLine().getStatusCode() == 200) {
 			System.out.println("Connected!");
-		}
 		
-		RedditJsonParser rjp = new RedditJsonParser();
-		
-		try {
-			JSONObject parsedJson = rjp.parseData(EntityUtils.toString(response.getEntity()));
-			headers.put("Authorization","bearer " + parsedJson.getString("access_token"));
-			CloseableHttpResponse accessTokenResponse =  HttpHandler.get("https://oauth.reddit.com/api/v1/me", headers);
+			RedditJsonParser rjp = new RedditJsonParser();
 			
-		} catch (ParseException | IOException | JSONException e) {
-			e.printStackTrace();
+			try {
+				JSONObject parsedJson = rjp.parseData(EntityUtils.toString(response.getEntity()));
+				headers.put("Authorization","bearer " + parsedJson.getString("access_token"));
+				CloseableHttpResponse accessTokenResponse =  HttpHandler.get("https://oauth.reddit.com/api/v1/me", headers);
+				accessToken = parsedJson.getString("access_token");
+				
+			} catch (ParseException | IOException | JSONException e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("Failed to connect to reddit\n" + response.getStatusLine());
 		}
 	}
 }
