29bf0ace6d94e6a81d000b17e163dfd26b7deddd
Adam King <a.king.per@Gmail.com>
2016-03-06 12:13:17 +0000

    Started work on adding posting submissions to reddit

--- src/com/snessy/main/Application.java
+++ src/com/snessy/main/Application.java
@@ -2,11 +2,24 @@ package com.snessy.main;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.http.ParseException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import com.snessy.auth.HttpHandler;
 import com.snessy.auth.RedditConnector;
 import com.snessy.auth.WeatherConnector;
+import com.snessy.dao.SubredditDao;
 import com.snessy.database.Database;
+import com.snessy.parse.RedditJsonParser;
+import com.snessy.reddit.Subreddit;
 
 public class Application {
 
@@ -31,5 +44,53 @@ public class Application {
 		Database.getInstance().connect();
 		
 		WeatherConnector weatherConnector = new WeatherConnector(props.getProperty("weatherAPIKey"));
+		SubredditDao subredditDao = new SubredditDao();
+		
+		Timer timer = new Timer();
+		RedditJsonParser jsonParser = new RedditJsonParser();
+		
+		timer.schedule(new TimerTask(){
+			@Override
+			public void run() {
+				Subreddit[] subreddits = subredditDao.getAllEntries();
+				
+				if(subreddits != null) {
+					HashMap<String, String> headers = new HashMap<String, String>();
+					HashMap<String, String> urlNameValuePairs = new HashMap<String, String>();
+					
+					headers.put("authorization", "bearer "+redditConnector.getAccessToken());
+					headers.put("user-agent", RedditConnector.USER_AGENT);
+					
+					urlNameValuePairs.put("api_type", "json");
+					urlNameValuePairs.put("kind", "self");
+					urlNameValuePairs.put("sr", "codingresources");
+					urlNameValuePairs.put("sendreplies", "true");
+					urlNameValuePairs.put("text", "Test Post");
+					urlNameValuePairs.put("title", "TestPost");
+					urlNameValuePairs.put("url", "text");
+					
+					for(Subreddit sub : subreddits){
+						headers.put("sr", sub.getName());
+						CloseableHttpResponse response = HttpHandler.post("https://oauth.reddit"
+								+ ".com/api/submit", null, null, headers, urlNameValuePairs);
+						try {
+							JSONObject json = jsonParser.parseData(EntityUtils.toString(response.getEntity()));
+							System.out.println(json);
+						} catch (ParseException | IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}, 0 ,TimeUnit.HOURS.toMillis(24));
+		
+		timer.schedule(new TimerTask(){
+
+			@Override
+			public void run() {
+				// Code to check messages and update database with new requests
+			}
+			
+		}, 0, TimeUnit.MINUTES.toMillis(5));
 	}
 }
